3d46e934a721dc51ab9540bc636da183d4995d9d
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2024-06-11 14:56:30 +0500

    Fragment SearchView
    Ishladi

--- app/src/main/java/com/example/mathterminology/MainFragment.java
+++ app/src/main/java/com/example/mathterminology/MainFragment.java
@@ -1,83 +1,65 @@
 package com.example.mathterminology;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+//import android.view.MenuItem;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
+
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
 
-/**
-// * A simple {@link Fragment} subclass.
-// * Use the {@link MainFragment#newInstance} factory method to
-// * create an instance of this fragment.
- */
+
+
 public class MainFragment extends Fragment {
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public MainFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment MainFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static MainFragment newInstance(String param1, String param2) {
-//        MainFragment fragment = new MainFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//
-//
-//    }
+
 
     RecyclerView rview;
     myadapter adapter;
+
+    MenuItem menuItem;
+    SearchView searchView;
+    Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
+
         View view =    inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+        toolbar = view.findViewById( R.id.toolbar );
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+
+        activity.setSupportActionBar( toolbar );
+        activity.getSupportActionBar().setTitle("");
+
+
 
 
 
@@ -94,6 +76,39 @@ public class MainFragment extends Fragment {
     }
 
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_item,menu);
+        menuItem = menu.findItem(R.id.search_1);
+        searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setIconified(true);
+        searchView.setQueryHint("Qidiruv");
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                processSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                processSearch(newText);
+                return true;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
 
 
@@ -137,7 +152,7 @@ public class MainFragment extends Fragment {
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
-//        ((AppCompatActivity)getActivity()). getMenuInflater().inflate(R.menu.menu_item,menu);
+//       getMenuInflater().inflate(R.menu.menu_item,menu);
 //
 //        MenuItem item = menu.findItem(R.id.search_1);
 //
